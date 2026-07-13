db03c8d29661ea4f78eea73eaa270905e4a48d29
ustc.hj@gmail.com <ustc.hj@gmail.com>
2026-07-13 10:57:31 +0800

    Update all packages to the latest versions.

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -2,12 +2,17 @@ import { Window } from 'happy-dom'
 
 const window = new Window()
 
-global.document = window.document as unknown as Document
-global.window = window as unknown as Window & typeof globalThis
-global.HTMLElement = window.HTMLElement as unknown as typeof HTMLElement
-global.Element = window.Element as unknown as typeof Element
-global.Node = window.Node as unknown as typeof Node
-global.MouseEvent = window.MouseEvent as unknown as typeof MouseEvent
-global.KeyboardEvent = window.KeyboardEvent as unknown as typeof KeyboardEvent
-global.Event = window.Event as unknown as typeof Event
-global.getSelection = () => window.getSelection()
+// happy-dom's Window/Selection types diverge structurally from the DOM lib
+// types, so we assign through Object.assign and cast the selection return.
+Object.assign(globalThis, {
+  document: window.document,
+  window,
+  HTMLElement: window.HTMLElement,
+  Element: window.Element,
+  Node: window.Node,
+  MouseEvent: window.MouseEvent,
+  KeyboardEvent: window.KeyboardEvent,
+  Event: window.Event,
+})
+
+globalThis.getSelection = () => window.getSelection() as unknown as Selection
